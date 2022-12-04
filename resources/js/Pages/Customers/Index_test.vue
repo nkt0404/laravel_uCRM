9317c1a33a4fb2cec8db2d1f3418cae76a8e4f7d
nkt0404 <nktorun7001@gmail.com>
2022-12-05 04:15:33 +0900

    customer regist

---
+++ resources/js/Pages/Customers/Index_test.vue
@@ -0,0 +1,22 @@
+<script setup>
+//import { onMounted } from 'vue';
+import Pagination from '@/Components/Pagination.vue';
+
+defineProps({
+  customers: Object
+})
+
+// onMounted(() => {
+//   console.log(props.customers)
+//   console.log(props.customers.last_page)
+// })
+
+</script>
+
+<template>
+  <ul v-for="customer in customers.data" :key="customer.id">
+    <li>{{ customer.id }}</li>
+    <li>{{ customer.name }}</li>
+  </ul>
+  <Pagination class="mt-6" :links="customers.links"></Pagination>
+</template>
